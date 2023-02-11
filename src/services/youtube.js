4590207985a0319c90f853a557857c11d62af5f7
Leandro Corso <contato@leandrocorso.com.br>
2023-02-11 11:55:33 -0300

    feat: allow just embedded videos

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -6,8 +6,9 @@ const key = process.env.REACT_APP_YT_API_KEY;
 async function fetchAll() {
   const part = `part=${["id", "snippet"].join("&part=")}`;
   const chart = "chart=mostPopular";
+  const embeded = "videoEmbeddable=true";
   const maxResults = "maxResults=13";
-  const params = [part, chart, maxResults, `key=${key}`].join("&");
+  const params = [part, chart, embeded, maxResults, `key=${key}`].join("&");
 
   try {
     return await axios.get(`${baseUrl}/videos?${params}`);
@@ -20,7 +21,8 @@ async function search(keyword) {
   const part = `part=${["id", "snippet"].join("&part=")}`;
   const maxResults = "maxResults=13";
   const type = "type=video";
-  const params = [part, maxResults, type, `key=${key}`].join("&");
+  const embeded = "videoEmbeddable=true";
+  const params = [part, maxResults, type, embeded, `key=${key}`].join("&");
 
   try {
     return await axios.get(`${baseUrl}/search?q=${keyword}&${params}`);
